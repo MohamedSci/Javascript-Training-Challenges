79b5a1d6c628dbe1b54f4b2b5fcc175da929c233
Mohamed Said Ibrahim <muhammedsaidsyed215@gmail.com>
2024-11-23 15:55:47 +0200

    33.getTheMaxiumSalesPerDays.js

--- 32.removeDuplicateCharacters.js
+++ 32.removeDuplicateCharacters.js
@@ -1,9 +1,16 @@
+
 function removeDuplicateCharacters(string) {
-    return string
-      .split('')
-      .filter(function(item, pos, self) {
-        return self.indexOf(item) == pos;
-      })
-      .join('');
-  }
-  console.log(removeDuplicateCharacters('baraban'));
+  return string
+    .split('') // Convert the string into an array of characters
+    .filter((item, pos, self) => self.indexOf(item) === pos) // Keep only unique characters
+    .join(''); // Join the unique characters back into a string
+}
+
+console.log(removeDuplicateCharacters('baraban')); // Output: "barn"
+
+
+// Explanation:
+
+// Splitting the String: We break the string into individual characters.
+// Filtering Unique Characters: The filter() method keeps only characters that appear at their first index.
+// Joining the Unique Characters: Finally, we combine the unique characters back into a string.
